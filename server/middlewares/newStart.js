b8bcf580c05fc75b7513e6ee98675b462722c9a1
Nguyen <doan88nguyen@gmail.com>
2022-06-19 06:29:06 +0200

    Drop & resync

--- server/middlewares/newStart.js
+++ server/middlewares/newStart.js
@@ -8,17 +8,17 @@ function newStart() {
 
     db.sequelize
   .sync({
-    //force: true
-    alter: true
+    force: true
+    //alter: true
   })
   .then(() => {
     console.log(
-      //"Drop and Resync Db"
+      "Drop and Resync Db"
       //"No Drop & Resync"
-      "Alter & Resync"
+      //"Alter & Resync"
     );
-    //initial();
-    //createAdmin();
+    initial();
+    createAdmin();
     
   });
 }
